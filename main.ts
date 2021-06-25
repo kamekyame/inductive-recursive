38955ab9fc77bb56a6bfe8ede3942a3f6e09bc85
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2021-06-25 16:25:05 +0900

    add main.ts

---
+++ main.ts
@@ -0,0 +1,22 @@
+// a[1] = 1
+// a[n] = 2a[n-1] + 1
+
+function inductive(num: number) {
+  if (num < 1) throw Error("'num' is not more than 0");
+  let n = 1;
+  for (let i = 2; i <= num; i++) {
+    n = 2 * n + 1;
+  }
+  return n;
+}
+
+function recursive(num: number): number {
+  if (num < 1) throw Error("'num' is not more than 0");
+
+  if (num === 1) return 1;
+  else return 2 * recursive(num - 1) + 1;
+}
+
+for (let i = 1; i <= 15; i++) {
+  console.log(i, inductive(i), recursive(i));
+}
